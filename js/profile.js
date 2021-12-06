c90c15991f98687da20f7c66c002b2c74af8ffbd
Jayden-Carstensen <78876259+Jayden-Carstensen@users.noreply.github.com>
2021-12-06 15:43:19 -0600

    Add files via upload

---
+++ js/profile.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+const hamburger = document.querySelector(".ham");  
+ const navsub = document.querySelector(".nav-sub");  
+ hamburger.addEventListener('click', () => {  
+  hamburger.classList.toggle("change")  
+  navsub.classList.toggle("nav-change")  
+ });  
